fbc0bf5e619b26ecb02c9be7f1fa6a8a5d3107df
pogorelov.a <pogorelov.a@azumail.ru>
2019-02-19 09:42:01 +0500

    Добавьте файлы проекта.

---
+++ MapsExample/MapsExample.Android/MyApp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MapsExample.Droid
+{
+    [Application]
+    [MetaData("com.google.android.maps.v2.API_KEY",
+               Value = key)]
+    public class MyApp : Application
+    {
+        public MyApp(IntPtr javaReference, JniHandleOwnership transfer)
+            : base(javaReference, transfer)
+        {
+
+        }
+    }
+}
